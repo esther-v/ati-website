100f7e144fa8ce5b3c656eeea9de7bfb81013053
esther-v <esvalas@gmail.com>
2024-02-08 19:56:44 +0100

    add navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='nav'>
+      ATI
+      <div className="menu">
+        <a>Accueil</a>
+        <a>Prestations</a>
+        <a>Réalisations</a>
+        <a>Contact</a>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
